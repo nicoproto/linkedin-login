a40b025e3aeebb0e6d699654144019bba2a24196
Nico Proto <nicolasproto@gmail.com>
2020-10-18 12:03:51 +0300

    Configured omniauth on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, and :trackable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  devise :omniauthable, omniauth_providers: %i[linkedin]
 end
