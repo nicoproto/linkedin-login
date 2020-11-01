461f8f210b976529ab5580af314709e3a10ad576
Nico Proto <nicolasproto@gmail.com>
2020-11-01 19:29:29 +0300

    Updated credentials to Rails Credentials

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,7 +272,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :linkedin, ENV['LINKEDIN_API_ID'], ENV['LINKEDIN_API_KEY']
+  config.omniauth :linkedin, Rails.application.credentials[Rails.env.to_sym][:LINKEDIN_API_ID], Rails.application.credentials[Rails.env.to_sym][:LINKEDIN_API_KEY]
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
